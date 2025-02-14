a5147014b2afb4d2eedc5885011ad44a1a7c10e6
AdityaKatare <63633617+AdityaKatare@users.noreply.github.com>
2025-02-14 21:19:22 +0530

    Add files via upload

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "efdc3c5dd92fa0d611d148643aa5c5f0";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
